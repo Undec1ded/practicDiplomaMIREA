4fa76cd1edf3a35e907967a8bb1afb3397d20593
Undec1ded <80984117+Undec1ded@users.noreply.github.com>
2023-11-27 21:20:17 +0300

    Add files via upload
    added new song

--- music_top.sv
+++ music_top.sv
@@ -1,10 +1,13 @@
 `timescale 1ns / 1ps
 
+//top module
+
 module music_top#(
     bits_data_out = 1024
 )(
     input clk,
     input button_action,
+    input [3 : 0] sw,
     
     output signal,
     output sync,
@@ -15,7 +18,7 @@ logic [bits_data_out  - 1 : 0] data;
 wire [5 : 0] note_state_wire;
 
 ram_sinusoid ram_sinusoid(.clk(clk), .data(data));
-unravel unravel(.clk(clk), .button_action(button_action), .note(note_state_wire));
+unravel unravel(.clk(clk), .button_action(button_action), .note(note_state_wire), .sw(sw));
 spi_master spi_master(.clk(clk), .button_action(button_action), .data(data), .note_state(note_state_wire), .mosi(signal), .sync(sync), .sclk(sclk));    
     
 endmodule
--- spi_master.sv
+++ spi_master.sv
@@ -43,7 +43,7 @@ module spi_master#(
      
     )(
 input clk,
-input logic [3 : 0] note_state,
+input logic [5 : 0] note_state,
 input button_action,
 input logic [bit_data - 1 : 0] data,
 
@@ -90,20 +90,67 @@ end
 
 always@(posedge sclk) begin 
     case (note_state)
-        4'b0001: frequency_note = 888; // frequency_A1
-        4'b0010: frequency_note = 838; // frequency_Ah1
-        4'b0011: frequency_note = 444; // frequency_A2 
-        4'b0100: frequency_note = 419; // frequency_Ah2
-        4'b0101: frequency_note = 747; // frequency_C2
-        4'b0110: frequency_note = 373; // frequency_C3
-        4'b0111: frequency_note = 665; // frequency_D2
-        4'b1000: frequency_note = 628; // frequency_Dh2
-        4'b1001: frequency_note = 333; // frequency_D3
-        4'b1010: frequency_note = 560; // frequency_F2
-        4'b1011: frequency_note = 279; // frequency_F3
-        4'b1100: frequency_note = 996; // frequency_G1
-        4'b1101: frequency_note = 498; // frequency_G2
-        4'b1110: frequency_note = 249; // frequency_G3
+        6'b000001: frequency_note = 888; // frequency_A1 /628 /333
+        6'b000010: frequency_note = 838; // frequency_Ah1
+        6'b000011: frequency_note = 444; // frequency_A2 
+        6'b000100: frequency_note = 419; // frequency_Ah2
+        6'b000101: frequency_note = 747; // frequency_C2
+        6'b000110: frequency_note = 373; // frequency_C3
+        6'b000111: frequency_note = 665; // frequency_D2
+        6'b001010: frequency_note = 560; // frequency_F2
+        6'b001011: frequency_note = 279; // frequency_F3
+        6'b001100: frequency_note = 996; // frequency_G1
+        6'b001101: frequency_note = 498; // frequency_G2
+        6'b001110: frequency_note =  249; // frequency_G3
+
+        6'b001111: frequency_note = 1496;
+        6'b010000: frequency_note = 1410;
+        6'b010001: frequency_note = 705;
+        6'b010010: frequency_note = 352;
+        6'b010011: frequency_note = 187;
+        6'b010100: frequency_note = 176;
+        6'b010101: frequency_note = 1333;
+        6'b010111: frequency_note = 1256;
+        6'b011000: frequency_note = 628;
+        6'b011001: frequency_note = 333;
+        6'b011010: frequency_note = 319;
+        6'b011011: frequency_note = 166;
+        6'b011100: frequency_note = 157;
+        6'b011101: frequency_note = 1187;
+        6'b011110: frequency_note = 593;
+        6'b011111: frequency_note = 296;
+        6'b100000: frequency_note = 148;
+        6'b100001: frequency_note = 1119;
+        6'b100010: frequency_note = 1059;
+        6'b100011: frequency_note = 529;
+        6'b100101: frequency_note = 264;
+        6'b100110: frequency_note = 140;
+        6'b100111: frequency_note = 132;
+        6'b100111: frequency_note = 941;
+        6'b101000: frequency_note = 471;
+        6'b101001: frequency_note = 235;
+        6'b101010: frequency_note = 187;
+        6'b101011: frequency_note = 177;
+        6'b101100: frequency_note = 227;
+        6'b101101: frequency_note = 210;
+        6'b101110: frequency_note = 114;
+        6'b101111: frequency_note = 105;
+        6'b110000: frequency_note = 792;
+        6'b110001: frequency_note = 396;
+        6'b110010: frequency_note = 198;
+        6'b110011: frequency_note = 99;
+        6'b110100: frequency_note = 3004;
+        6'b110101: frequency_note = 2831;
+        6'b110110: frequency_note = 2657;
+        6'b110111: frequency_note = 2520;
+        6'b111000: frequency_note = 2382;
+        6'b111001: frequency_note = 2245;
+        6'b111010: frequency_note = 2111;
+        6'b111011: frequency_note = 1993;
+        6'b111100: frequency_note = 1887;
+        6'b111101: frequency_note = 1776;
+        6'b111110: frequency_note = 1677;
+        6'b111111: frequency_note = 1588;
     default : frequency_note = frequency_25MGz;  
     endcase  
     
